83457610714287c6d0da8ea0d8e9c833649a590c
Tom <thomas@seennothurd.com>
2014-01-13 09:15:22 +0200

    Finish layout for image pager

--- wp-content/themes/polytope/functions.php
+++ wp-content/themes/polytope/functions.php
@@ -188,7 +188,6 @@ remove_action( 'woocommerce_before_shop_loop_item_title', 'woocommerce_template_
 remove_action( 'woocommerce_before_shop_loop_item_title', 'woocommerce_show_product_loop_sale_flash', 10 );
 
 function polytope_wc_add_slider_pager() {
-	echo 'boo';
 	get_template_part( 'woocommerce/shop-loop-image', 'pager' );
 }
 add_action( 'woocommerce_after_shop_loop', 'polytope_wc_add_slider_pager', 10 );
--- wp-content/themes/polytope/woocommerce/shop-loop-image-pager.php
+++ wp-content/themes/polytope/woocommerce/shop-loop-image-pager.php
@@ -10,17 +10,19 @@
 global $product;
 ?>
 
-<section class="product-shortcuts">
-  <h4 class="more-photos"><?php _e('More seats', 'wpsc'); ?></h4>
-  <div class="thumbnail-container pager">
-    <?php 
-      $count = 1; 
-      while ( have_posts() ) : the_post();
-    ?>
-      <a href="#<?php echo $count; $count = $count + 1; ?>" class="product-shortcut">
-        <!-- <img class="product-thumbnail" id="product_image_<?php //echo wpsc_the_product_id(); ?>" alt="<?php //echo wpsc_the_product_title(); ?>" title="<?php //echo wpsc_the_product_title(); ?>" src="<?php //echo esc_url( wpsc_the_product_image() ); ?>"/> -->
-        <?php echo woocommerce_template_loop_product_thumbnail(); ?>
-      </a>
-    <?php endwhile; ?>
+<section class="slider-pager-container">
+  <div class="slider-pager">
+    <h4 class="more-photos"><?php _e('More seats', 'wpsc'); ?></h4>
+    <div class="thumbnail-container pager">
+      <?php 
+        $count = 1; 
+        while ( have_posts() ) : the_post();
+      ?>
+        <a href="#<?php echo $count; $count = $count + 1; ?>" class="product-shortcut">
+          <!-- <img class="product-thumbnail" id="product_image_<?php //echo wpsc_the_product_id(); ?>" alt="<?php //echo wpsc_the_product_title(); ?>" title="<?php //echo wpsc_the_product_title(); ?>" src="<?php //echo esc_url( wpsc_the_product_image() ); ?>"/> -->
+          <?php echo woocommerce_template_loop_product_thumbnail(); ?>
+        </a>
+      <?php endwhile; ?>
+    </div>
   </div>
 </section>
